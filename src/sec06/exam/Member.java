10b8c4ce3dc1bb5433e6f7b724996e4e7a2e2221
kukja <kukja@DESKTOP-OV2ABN2>
2020-09-13 23:48:51 +0900

    2020-09-13

---
+++ src/sec06/exam/Member.java
@@ -0,0 +1,13 @@
+package sec06.exam;
+
+public class Member {
+	
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	public Member(String name, String id) {}
+	
+	public Member(String name, String id, String password, int age) {}
+}
---
+++ src/sec06/exam/Printer.java
@@ -0,0 +1,17 @@
+package sec06.exam;
+
+public class Printer {
+	 
+	int num;
+	boolean result;
+	double dom;
+	String name;
+	
+	public void outCom() {
+		// TODO Auto-generated method stub
+		System.out.println(num);
+		System.out.println(result);
+		System.out.println(dom);
+		System.out.println(name);
+	}
+}
